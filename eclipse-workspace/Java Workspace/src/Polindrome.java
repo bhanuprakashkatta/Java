58fa3ce092681e76cfd12d88d7398bffde8d6bf2
bhanuprakash katta <bhanuprakashkatta@gmail.com>
2018-05-02 16:25:45 -0500

    Polindrome

---
+++ eclipse-workspace/Java Workspace/src/Polindrome.java
@@ -0,0 +1,61 @@
+public class Polindrome {
+
+	public Polindrome() {
+		// TODO Auto-generated constructor stub
+	}
+
+	static	String name="borrow or rob";
+	static String original="";
+	static String reverse="";
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println    ("Given String is----------> "+name);
+		originalWordSpacesChangingMethod(name);
+		reverseChangingMethod(name);
+		
+		
+		if (original.equals(reverse)) {
+			System.out.println("--Entered string is polindrome--");
+		}
+		else
+			System.out.println("---Entered string is not polindrome---");
+
+	}
+
+	
+		
+
+	private static void originalWordSpacesChangingMethod(String name) {
+		// TODO Auto-generated method stub
+		
+		for(int j=0; j<name.length();j++) {
+			char temp=name.charAt(j);
+			if (temp==' ') {
+				continue;
+			}
+			original=original.concat(String.valueOf(temp));			
+		}
+		
+	}
+
+	private static void reverseChangingMethod(String name) {
+		// TODO Auto-generated method stub
+		
+		for(int i=name.length(); i>0;i--)
+		{
+			char temp=name.charAt(i-1);
+			if(temp==' ')
+			{
+				continue;
+			}
+			reverse=reverse.concat(String.valueOf(temp));
+		}
+		System.out.println     ("Reversed string is--------> "+reverse);
+		
+	}
+
+	
+
+}
