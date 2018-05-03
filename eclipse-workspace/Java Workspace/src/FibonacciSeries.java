1d04e5dbfa02768843a11e5a4996b1c0e4595517
bhanuprakash katta <bhanuprakashkatta@gmail.com>
2018-05-03 10:31:40 -0500

    fibonacci update

--- eclipse-workspace/Java Workspace/src/FibonacciSeries.java
+++ eclipse-workspace/Java Workspace/src/FibonacciSeries.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class FibonacciSeries {
 	static int number1=0;
@@ -8,20 +9,26 @@ public class FibonacciSeries {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Upto which number to print fibonacci series");
+		Scanner sc=new Scanner(System.in);
+		int number=sc.nextInt();
 		System.out.println(number1+","+number2);
-		fibonacciMethod();
+		fibonacciMethod(number);
 
 	}
 
-	private static void  fibonacciMethod() {
+	private static void  fibonacciMethod(int number) {
 		// TODO Auto-generated method stub
 		
-		for(int i=2;i<=10;i++)
+		for(int i=2;i<=number;i++)
 		{
 				int number3=number1+number2;
+				if (number3<=number)
+				{
 			System.out.println(number3);
 			number1=number2;
 			number2=number3;
+				}
 			/*int number4=number1+number2;
 			System.out.println(number4);
 			number2=number4;*/
